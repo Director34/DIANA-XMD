52c6f9f73a3c774ca4fbf689ce6c94e62d35aa5b
Fredi Ai <frediezra0@gmail.com>
2025-08-12 20:12:24 +0300

    🎸

---
+++ plugins/ping.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const { ezra } = require("../fredi/ezra");
+const axios = require('axios');
+const moment = require("moment-timezone");
+const set = require(__dirname + '/../set');
+moment.tz.setDefault('' + set.TIMEZONE);
+
+
+ezra({
+  'nomCom': "ping",
+  'categorie': "General-Fredi"
+}, async (_0x12a838, _0x2d8d4e, _0x1f0ba4) => {
+  let {
+    ms: _0x5d2f0c
+  } = _0x1f0ba4;
+  const {
+    time: _0xb5466b,
+    date: _0x4c687e
+  } = {
+    'time': moment().format("HH:mm:ss"),
+    'date': moment().format("DD/MM/YYYY")
+  };
+  const _0x4950ba = Math.floor(Math.random() * 0x64) + 0x1;
+  try {
+    await _0x2d8d4e.sendMessage(_0x12a838, {
+      'audio': {
+        'url': "https://files.catbox.moe/se9mii.mp3"
+      },
+      'mimetype': "audio/mp4",
+      'ptt': true,
+      'contextInfo': {
+        'isForwarded': true,
+        'forwardedNewsletterMessageInfo': {
+          'newsletterJid': "120363421677960956@newsletter",
+          'newsletterName': "𝐃𝐈𝐀𝐍𝐀 ~ 𝐗𝐌𝐃",
+          'serverMessageId': 0x8f
+        },
+        'forwardingScore': 0x3e7,
+        'externalAdReply': {
+          'title': "𝐃𝐈𝐀𝐍𝐀 ~ 𝐗𝐌𝐃",
+          'body': "⚫ Pong: " + _0x4950ba + "ms\n📅 *Date:* " + _0x4c687e + "\n⏰ *Time:* " + _0xb5466b,
+          'thumbnailUrl': "https://files.catbox.moe/wh3gx1.jpg",
+          'mediaType': 0x1,
+          'renderSmallThumbnail': true
+        }
+      }
+    }, {
+      'quoted': _0x5d2f0c
+    });
+  } catch (_0x1149fe) {
+    console.log("❌ Ping Command Error: " + _0x1149fe);
+    repondre("❌ Error: " + _0x1149fe);
+  }
+});
